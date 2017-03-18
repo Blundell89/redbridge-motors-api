bd1ca4b57549c8c5a9e7af39ab3e8355f913b184
Lee Blundell <leeblundell16@gmail.com>
2017-03-18 07:07:20 +0000

    Image push

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -2,5 +2,9 @@ export default Object.seal({
     data: {
         mongoUrl: process.env.mongoUrl || 'mongodb://localhost/redbridge-motors',
         vehicleCollection: 'vehicles'
+    },
+    fileStorage: {
+        connectionString: process.env.storageConnectionString || 'UseDevelopmentStorage=true',
+        publicAssetsContainer: 'public-assets'
     }
 });
--- src/services/images/image.service.ts
+++ src/services/images/image.service.ts
@@ -1,5 +1,23 @@
+import * as Azure from 'azure-storage';
+import Constants from '../constants';
+import { uuid } from 'uuid/v4';
+
 export class ImageService {
+  
+
   public async create(buffer: Buffer, contentType: string): Promise<string> {
-    return await Promise.resolve('http://www.google.co.uk');
+    let container = Constants.fileStorage.publicAssetsContainer;
+
+    const blobService = Azure.createBlobService(Constants.fileStorage.connectionString);
+    blobService.createContainerIfNotExists(container, {
+      publicAccessLevel: 'blob'
+    }, (err, result) => {
+      if (err)
+        throw err.message;
+    });
+
+    blobService.createBlockBlobFromStream(container, uuid.v4(), buffer.buffer, 
+
+    //Return uri
   }
 }
